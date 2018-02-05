44a75270cc99fcd2e51d0eee3d77dac1708e1b51
pv16061995 <pv16061995@gmail.com>
2018-02-05 11:21:28 +0530

    update utility helper

---
+++ application/helpers/fronthelper/utility_helper.php
@@ -0,0 +1,70 @@
+<?php 
+
+function asset_url()
+{
+   return base_url().'assets/';
+}
+
+function logo_url()
+{
+	return base_url().'assets/images/logo.png';
+}
+
+function favicon_url()
+{
+	return base_url().'assets/images/favicon.png';
+}
+
+function project_name()
+{
+	return 'Binance';
+}
+
+function kyc_url()
+{
+	return base_url().'kyc/'
+}
+
+function maintenace_url()
+{
+	return base_url().'maintenace/'
+}
+
+function notification_url()
+{
+	return base_url().'notification/'
+}
+
+function payment_url()
+{
+	return base_url().'payment/'
+}
+
+function referal_url()
+{
+	return base_url().'referal/'
+}
+
+function support_url()
+{
+	return base_url().'support/'
+}
+
+function tfauth_url()
+{
+	return base_url().'tfauth/'
+}
+
+function trading_url()
+{
+	return base_url().'trading/'
+}
+
+function wallet_url()
+{
+	return base_url().'wallet/'
+}
+
+
+
+?>
